255bc96f7244fd5992db19a55c7cfcc30e3b2fd2
Yehudi4 <abdielm678@gmail.com>
2024-04-30 20:36:55 -0600

    primer cambio confirmado

---
+++ ArrayInverso.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class ArrayInverso {
+
+    public static int tamv = 0;
+    public static Scanner leer = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+
+        boolean condiciosalida = true;
+
+        do {
+
+            try {
+
+                System.out.println(
+                        "Ingrese el tamaño del arreglo que no supere los 16 y sea mayor a 1 y que tampoco sea negativo ");
+                tamv = leer.nextInt();
+                condiciosalida = (tamv >= 2 && tamv <= 16) ? false : true;
+
+            } catch (Exception e) {
+                System.out.println(e);
+                System.out.println("Ocurrio algo revisar");
+                leer.nextLine();
+            }
+
+        } while (condiciosalida);
+
+        //recibimos el array del metodo
+        int array[] = llenado();
+
+        System.out.println("Mostrar");
+
+        MostrarInvertido(array);
+
+    }
+
+    public static int[] llenado() {
+
+        int[] arreglo = new int[tamv];
+
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("ingrese el valor [" + i + "]");
+            arreglo[i] = leer.nextInt();
+        }
+
+        return arreglo;
+    }
+
+    public static void MostrarInvertido(int[] a) {
+
+        for (int i = a.length - 1; i >= 0; i--) {
+            System.out.println(a[i]);
+        }
+
+    }
+
+}
